c5d8a856ee9ba01b0ae2d749f9090bf0e441513e
Oluwadamilare <Oluwadamilare@DESKTOP-RDBREH0>
2020-12-22 15:08:04 +0100

    This is first push of this project

---
+++ LoginUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NSETRADEREPORT.MODEL.Models.DB
+{
+    public partial class LoginUser
+    {
+        public long Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateLogin { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
---
+++ Permission.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NSETRADEREPORT.MODEL.Models.DB
+{
+    public partial class Permission
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public int ModuleId { get; set; }
+        public int FormId { get; set; }
+        public bool CanAdd { get; set; }
+        public bool CanView { get; set; }
+        public bool CanUpdate { get; set; }
+        public bool CanDelete { get; set; }
+        public bool CanApprove { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public byte Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string RoleName { get; set; }
+        public string FormName { get; set; }
+        public string ModuleName { get; set; }
+
+        public Form Form { get; set; }
+        public Module Module { get; set; }
+        public Role Role { get; set; }
+    }
+}
